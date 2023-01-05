6e89f11bcfd6546446ff3b6d3426541778e45050
Rose Black <o2bablackrose@gmail.com>
2023-01-05 10:01:35 -0500

    Second commit

--- Develop/script.js
+++ Develop/script.js
@@ -1,93 +1,144 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
-var uppers = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-var lowers = 'abcdefghijklmnopqrstuvwxyz';
-var nums = '0123456789';
-var specials = '@%+\/!#$^?:(){}[]~-_.';
+var timerE1 = document.getElementById('countdown');
+var timerE2 = document.getElementById('countdowntwo');
+var questionE1 = document.getElementById('thequestion');
+var answerE1 = document.getElementById('a');
+var answerE2 = document.getElementById('b');
+var answerE3 = document.getElementById('c');
+var answerE4 = document.getElementById('d');
+var labelE1 = document.getElementById('alabel');
+var labelE2 = document.getElementById('blabel');
+var labelE3 = document.getElementById('clabel');
+var labelE4 = document.getElementById('dlabel');
+var labelE5 = document.getElementById('thescore');
+var infoE1 = document.getElementById('initials');
+var buttonE1 = document.getElementById('submitit');
+var notificationE1 = document.getElementById('notification');
+var timeLeft = 90;
+var getSelectedValue = document.querySelector( 'input[name="answer"]:checked');
+var startpoint = 0;
+var finaltime = 0;
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+var elements = [
+  {
+    "question": "Commonly used data types DO Not Include",
+    "answers": [
+      "1. strings",
+      "2. booleans",
+      "3. alerts",
+      "4. numbers"
+    ],
+    "correct": "c"
+  },
+  {
+    "question": "The condition in an if/else statement is enclosed with _____",
+    "answers": [
+      "1. quotes",
+      "2. curly brackets",
+      "3. parentheses",
+      "4. square brackets"
+    ],
+    "correct": "b"
+  },
+  {
+    "question": "Arrays in JavaScript can be used to store _____",
+    "answers": [
+      "1. numbers and strings",
+      "2. other arrays",
+      "3. booleans",
+      "4. all of the above"
+    ],
+    "correct": "d"
+  },
+  {
+    "question": "String values must be enclosed within _____ when being assigned to variables",
+    "answers": [
+      "1. commas",
+      "2. curly brackets",
+      "3. quotes",
+      "4. parentheses"
+    ],
+    "correct": "c"
+  },
+  {
+    "question": "A very useful tool used during development and debugging for printing content to the debugger is: ",
+    "answers": [
+      "1. JavaScript",
+      "2. terminal/bash",
+      "3. for loops",
+      "4. console log"
+    ],
+    "correct": "d"
+  }
+]
+
+function countdown() {
+    var timeInterval = setInterval(function () {
+        if (timeLeft >= 1) {
+          timerE1.textContent = timeLeft + ' seconds remaining';
+          timeLeft--;
+        }  else {
+          timerE1.textContent = '';
+          clearInterval(timeInterval);
+        }
+      }, 1000);
+    }
+
+function askQuestions() {
+  for(var k = 0; k <= 4; k++) {
+    questionE1.textContent = elements[startpoint].question;
+    labelE1.textContent = elements[startpoint].answers[0];
+    labelE2.textContent = elements[startpoint].answers[1];
+    labelE3.textContent = elements[startpoint].answers[2];
+    labelE4.textContent = elements[startpoint].answers[3];
+    }
+  }
   
-  passwordText.value = password;
+function checkAnswer() {
+  var answergiven = '';
+  if(document.getElementById('a').checked) {   
+    answergiven = 'a';
+    console.log(answergiven)
+  }   
+  else if(document.getElementById('b').checked) {   
+    answergiven = 'b';
+    console.log(answergiven);
+  }   
+  else if(document.getElementById('c').checked) {   
+    answergiven = 'c';
+    console.log(answergiven);
+  }   
+else if(document.getElementById('d').checked) {   
+    answergiven = 'd';
+    console.log(answergiven)
+  }  
+else {   
+    answergiven = '';
+  }
+  if(answergiven == elements[startpoint].correct) {
+    notificationE1.textContent = "Correct!";
+  }
+  else{
+    notificationE1.textContent = "Wrong!"
+    timeLeft -= 10;
+  }
+  startpoint++;
+  if(startpoint < 5){
+    askQuestions();
+  }
+  else{
+    finaltime = timeLeft;
+    console.log("done");
+    console.log(finaltime);
+    window.location.href='enterdata.html';
+  //  timerE2.textContent='Time: '+ finaltime;
 
+  }
+  document.getElementById(answergiven).checked = false;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-function generatePassword() {
-  
-  var passlength = prompt("Password length between 8 and 128");
-  while ((passlength < 8)  || (passlength > 128) || (isNaN(passlength))){
-    passlength = prompt("Invalid length selected.  Password length between 8 and 128");
-  }
-  var categories = 0;
-
-  var lowercase = confirm("Lowercase characters required true/false");
-  if(lowercase == true)
-    categories++;
-
-  var uppercase = confirm("Uppercase characters required true/false");
-  if(uppercase == true)
-    categories++;
-
-  var numero = confirm("Numeric characters required true/false");
-  if(numero == true)
-    categories++;
-
-  var special = confirm("Special characters required true/false");
-  if(special == true)
-    categories++;
-
-  var allocated = Math.floor(passlength/categories);
-  let passphrase = '';
-  var mystart = 0;
-  
-  if(lowercase == true){
-    for(let k = mystart; k < allocated; k++){
-      passphrase += lowers.charAt(Math.floor(Math.random() * lowers.length));
-    }
-    mystart += allocated;
-    if((uppercase == false) && (numero == false) && (special == false)){
-      for(let m = mystart; m < passlength; m++)
-      {
-        passphrase += lowers.charAt(Math.floor(Math.random() * lowers.length));
-      }
-    }
-  }
-
-  if(uppercase == true){
-    for(let n = mystart; n < (allocated + mystart); n++){
-      passphrase += uppers.charAt(Math.floor(Math.random() * uppers.length));
-    }
-    mystart += allocated;
-    if((numero == false) && (special == false)){
-      for(let p = mystart; p < passlength; p++)
-      {
-        passphrase += uppers.charAt(Math.floor(Math.random() * uppers.length));
-      }
-    }
-  }
-
-  if(numero == true){
-    for(let q = mystart; q < (allocated + mystart); q++){
-      passphrase += nums.charAt(Math.floor(Math.random() * nums.length));
-    }
-    mystart += allocated;
-    if(special == false){
-      for(let r = mystart; r < passlength; r++)
-      {
-        passphrase += nums.charAt(Math.floor(Math.random() * nums.length));
-      }
-    }
-  }
-
-  if(special == true){
-    for(let t = mystart; t < passlength; t++){
-      passphrase += specials.charAt(Math.floor(Math.random() * specials.length));
-    }
-  }
-  return passphrase;
+function startQuiz() {
+  window.location.href='question.html'
 }
+  countdown();
+  askQuestions();
